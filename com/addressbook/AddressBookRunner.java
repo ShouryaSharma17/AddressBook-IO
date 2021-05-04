51f227ae772b1a96e1d442e25588b6cf453b9a54
ShouryaSharma17 <shouryasharma17@gmail.com>
2021-05-04 22:28:15 +0530

    Ability To Add Contact

--- com/addressbook/AddressBookRunner.java
+++ com/addressbook/AddressBookRunner.java
@@ -1,14 +1,21 @@
 package com.addressbook;
 import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Scanner;
+
+static HashMap<Integer,String> personDetails = new HashMap<>();
 
 public class AddressBookRunner {
     static HashSet<AddressBookSource> personDetails = new HashSet<>();
 
     public static void main(String[] args) {
         System.out.println("WELCOME TO THE ADDRESS BOOK PROGRAM");
-        AddressBookSource addPerson = new AddressBookSource();
-        addPerson.enterDetails();
-        personDetails.add(addPerson);
+        System.out.println("ENTER THE NUMBER OF CONTACTS TO ADD");
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        for(int i=0; i<choice;i++)
+            personDetails.put(i, new AddressBookSource().enterDetails());
         System.out.println(personDetails);
     }
 }
+
