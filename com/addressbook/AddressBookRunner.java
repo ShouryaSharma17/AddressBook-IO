db086ed0912dda0a700a9d31f0dd416b153c58cd
ShouryaSharma17 <shouryasharma17@gmail.com>
2021-05-05 19:37:49 +0530

    Ability To Edit Contacts

--- com/addressbook/AddressBookRunner.java
+++ com/addressbook/AddressBookRunner.java
@@ -2,20 +2,41 @@ package com.addressbook;
 import java.util.HashSet;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.ArrayList;
 
-static HashMap<Integer,String> personDetails = new HashMap<>();
 
 public class AddressBookRunner {
-    static HashSet<AddressBookSource> personDetails = new HashSet<>();
-
+    static ArrayList<AddressBookSource> personDetails = new ArrayList<>();
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("WELCOME TO THE ADDRESS BOOK PROGRAM");
-        System.out.println("ENTER THE NUMBER OF CONTACTS TO ADD");
-        Scanner scanner = new Scanner(System.in);
-        int choice = scanner.nextInt();
-        for(int i=0; i<choice;i++)
-            personDetails.put(i, new AddressBookSource().enterDetails());
-        System.out.println(personDetails);
+        boolean option = true;
+        while(option) {
+            System.out.println("1 for adding\n2 for editing\n3 for exit");
+            int input = scanner.nextInt();
+            switch (input) {
+                case 1 -> {
+                    System.out.println("ENTER THE NUMBER OF CONTACTS TO ADD");
+                    int choice = scanner.nextInt();
+                    for (int j = 0; j < choice; j++) {
+                        AddressBookSource person = new AddressBookSource();
+                        person.enterDetails();
+                        personDetails.add(person);
+                    }
+                    System.out.println(personDetails);
+                }
+                case 2 -> {
+                    System.out.println("Please enter the first name of the contact");
+                    String input1 = scanner.next();
+                    for (AddressBookSource i : personDetails) {
+                        if (i.getFirstName().equals(input1)) {
+                            i.editDetails();
+                        }else System.out.println("not found");
+                        System.out.println(personDetails);
+                    }
+                }
+                default -> option = false;
+            }
+        }
     }
 }
-
--- com/addressbook/AddressBookSource.java
+++ com/addressbook/AddressBookSource.java
@@ -30,9 +30,66 @@ public class AddressBookSource {
         System.out.println("ENTER THE PHONE NUMBER :");
         this.phoneNumber= scanner.nextLine();
     }
+
+    public  void editDetails(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("What details you want to edit: ");
+        System.out.println("1 for firstname\n2 for last name\n3 for address\n4 for city\n5 for state\n6 for email\n7 for zip code\n8 for phone number\n9 for exit");
+        int choice = scanner.nextInt();
+        switch(choice){
+            case 1:
+                System.out.println("first name to be edited to ");
+                String fName = input.nextLine();
+                this.firstName = fName;
+                break;
+
+            case 2:
+                System.out.println("last name to be edited to");
+                String lName = input.nextLine();
+                this.lastName = lName;
+                break;
+            case 3:
+                System.out.println("address to be edited to");
+                String newAddress = input.nextLine();
+                this.address = newAddress;
+                break;
+            case 4:
+                System.out.println("City to be edited to");
+                String newCity = input.nextLine();
+                this.city = newCity;
+                break;
+            case 5:
+                System.out.println("State to be edited to");
+                String newState = input.nextLine();
+                this.state = newState;
+                break;
+            case 6:
+                System.out.println("email to be edited to");
+                String newEmail = input.nextLine();
+                this.email = newEmail;
+                break;
+            case 7:
+                System.out.println("zip code to be edited to");
+                String newZip = input.nextLine();
+                this.email = newZip;
+                break;
+            case 8:
+                System.out.println("phone number to be edited to");
+                String newPhone = input.nextLine();
+                this.email = newPhone;
+                break;
+
+            default:
+                break;
+        }
+    }
+
+
+
+
     @Override
     public String toString() {
-        return "\nAddressBookSource{" +
+        return "AddressBookSource{" +
                 "\nfirstName='" + firstName + '\'' +
                 " \nLastName='" + lastName + '\'' +
                 " \naddress='" + address + '\'' +
@@ -43,4 +100,13 @@ public class AddressBookSource {
                 " \nphoneNumber='" + phoneNumber + '\'' +
                 '}';
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
 }
